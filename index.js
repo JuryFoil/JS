006fc528070c9a2b435d451af3413fca43d9f7a3
JuryFoil <mgarcia14@ucol.mx>
2023-10-04 11:16:27 -0600

    primera version

---
+++ index.js
@@ -0,0 +1,15 @@
+var numero=5
+var numero=4
+const constante=10
+let nombre= "Monica"
+let apellido= "Hernandez"
+let seleccion = document.querySelector("#Seccion1")
+//seleccion.innerHTML = `<h3> Mi nombre es Monica </h3>`
+
+alert('hola desde una alerta javascript')
+console.log('esto es JS desde HTML')
+console.log (numero)
+console.log (constante)
+console.log (nombre+" "+apellido)
+console.log (seleccion)
+
